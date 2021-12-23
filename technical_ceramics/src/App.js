b09dddd8a4015e0683b55083587d6b6b6d86a7f1
BearkillerPT <gilteixeira@ua.pt>
2021-12-23 19:06:43 +0000

    tried to update to work on smaller devices and small typos correct

--- technical_ceramics/src/App.js
+++ technical_ceramics/src/App.js
@@ -5,13 +5,12 @@ import * as d3 from "d3";
 import { sankey, sankeyLinkHorizontal } from 'd3-sankey'
 import data from './data.json'
 import chroma from "chroma-js";
-
-const width = 600, height = 400
+const { innerWidth: width, innerHeight: height } = window;
 
 const SankeyNode = ({ name, x0, x1, y0, y1, color }) => (
   <>
-    <rect x={x0} y={y0} width={x1 - x0} height={y1 - y0} fill={color}/>
-    <text x={x0 < width / 2 ? x1 + 6 : x0 - 40} y={((y1 + y0) / 2)+5} fontSize={15} fill={"black"}>{name}</text>
+    <rect x={x0} y={y0} width={x1 - x0} height={y1 - y0} fill={color} />
+    <text x={x0 < width / 2 ? x1 + 6 : x0 - 40} y={((y1 + y0) / 2) + 5} fontSize={15} fill={"black"}>{name}</text>
 
   </>
 )
@@ -35,10 +34,10 @@ function App() {
   const [sankey1, setSakey1] = useState("Year")
   const [sankey2, setSakey2] = useState("Technology")
   const [dataGraph, setDataGraph] = useState(null)
-  useEffect(()=>{
-  setDataGraph(generateGraph([sankey1, sankey2], "RD (%)")) 
+  useEffect(() => {
+    setDataGraph(generateGraph([sankey1, sankey2], "RD (%)"))
   }, [sankey1, sankey2])
-  if(dataGraph) {
+  if (dataGraph) {
     const layout = sankey()(dataGraph)
     const color = chroma.scale("Set3").classes(dataGraph.nodes.length);
     const colorScale = d3.scaleLinear()
@@ -48,56 +47,62 @@ function App() {
       .nodeWidth(15)
       .nodePadding(10)
       .extent([[1, 1], [width - 1, height - 5]])(dataGraph);
-  
+
     return (
       <div className="App">
 
         <div class="sankeyGraph">
           <div class="GraphHeader">
-            <p class="GraphTitle">Aluvial Plot</p>
+            <p class="GraphTitle">Alluvial Plot</p>
           </div>
-        <svg width={width} height={height} style={{ padding: 50 }}>
-          <g style={{ mixBlendMode: 'multiply' }}>
-            {dataGraph.nodes.map((node, i) => (
-              <SankeyNode
-                {...node}
-                key={i}
-                color={color(colorScale(i)).hex()}
-              />
-            ))}
-            {dataGraph.links.map((link, i) => (
-              <SankeyLink
-                link={link}
-                key={i}
-                color={color(colorScale(link.source.index)).hex()}
-  
-              />
-            ))}
-          </g>
-        </svg>
-        <div class="graphControlls">
-          <select value={sankey1} class="paramSelect" onChange={key=>{setSakey1(key.currentTarget.value)}}>
-            {Object.keys(data[0]).map((key,i) => {
-            return <option value={key} key={i}>{key}</option>
-            })
-            }
-          </select>
-          <select value={sankey2} class="paramSelect" onChange={key=>{setSakey2(key.currentTarget.value)}}>
-          {Object.keys(data[0]).map((key,i) => {
-            if(key != sankey1)
-              return <option value={key} key={i}>{key}</option>
-            })
-            }
-          </select>
-        </div>
+          <svg class="Graph" width={width} height={height} style={{ padding: 50 }}>
+            <g style={{ mixBlendMode: 'multiply' }}>
+              {dataGraph.nodes.map((node, i) => (
+                <SankeyNode
+                  {...node}
+                  key={i}
+                  color={color(colorScale(i)).hex()}
+                />
+              ))}
+              {dataGraph.links.map((link, i) => (
+                <SankeyLink
+                  link={link}
+                  key={i}
+                  color={color(colorScale(link.source.index)).hex()}
+
+                />
+              ))}
+            </g>
+          </svg>
+          <div class="graphControlls">
+            <div>
+              <p>1st paramether</p>
+              <select value={sankey1} class="paramSelect" onChange={key => { setSakey1(key.currentTarget.value) }}>
+                {Object.keys(data[0]).map((key, i) => {
+                  return <option value={key} key={i}>{key}</option>
+                })
+                }
+              </select>
+            </div>
+            <div>
+              <p>2nd paramether</p>
+              <select value={sankey2} class="paramSelect" onChange={key => { setSakey2(key.currentTarget.value) }}>
+                {Object.keys(data[0]).map((key, i) => {
+                  if (key != sankey1)
+                    return <option value={key} key={i}>{key}</option>
+                })
+                }
+              </select>
+            </div>
+          </div>
         </div>
       </div>
-    );  
+    );
 
   }
-  else{
-    return(<div></div>);
-  }  
+  else {
+    return (<div></div>);
+  }
 }
 
 //"RD (%)" will be divided into 10 categories: 90, 91, .. 99
@@ -106,13 +111,13 @@ const generateGraph = (sankeyNodes, output_var) => {
   var nodes = []
   for (let i = 0; i < sankeyNodes.length - 1; i++) {
     for (let node of data) {
-      if(!Number(node[output_var]))
+      if (!Number(node[output_var]))
         continue
       let first = node[sankeyNodes[i]]
       let second = node[sankeyNodes[i + 1]]
-      if(sankeyNodes[i] != "Year" && Number(first) && first>10) 
+      if (sankeyNodes[i] != "Year" && Number(first) && first > 10)
         first -= first % 10
-      if(sankeyNodes[i+1] != "Year" && Number(second) && second>10) 
+      if (sankeyNodes[i + 1] != "Year" && Number(second) && second > 10)
         second -= second % 10
       if (!(nodes.includes("" + first)))
         nodes.push("" + first)
@@ -136,15 +141,15 @@ const generateGraph = (sankeyNodes, output_var) => {
     Object.keys(rels[first]).map(second => {
       let rel_val = rels[first][second].sum / rels[first][second].count
       links.push({ "source": nodes.indexOf("" + first), "target": nodes.indexOf("" + second), "value": rels[first][second].count })
-      
-      links.push({ "source": nodes.indexOf("" + second), "target": nodes.length + (Math.abs(90-rel_val)|0), "value": rels[first][second].count  })
+
+      links.push({ "source": nodes.indexOf("" + second), "target": nodes.length + (Math.abs(90 - rel_val) | 0), "value": rels[first][second].count })
 
     })
   })
   var res_nodes = []
   nodes.map(node => res_nodes.push({ "name": node }))
-  for(let i = 90; i < 100; i++)
-    res_nodes.push({"name": i + "%"})
+  for (let i = 90; i < 100; i++)
+    res_nodes.push({ "name": i + "%" })
   return { "nodes": res_nodes, "links": links }
 }
 
