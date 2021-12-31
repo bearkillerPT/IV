716429a3cbb1f9538aee43a3f7baf6cc2d6c689f
BearkillerPT <gilteixeira@ua.pt>
2021-12-31 12:58:53 +0000

    added aula1

---
+++ treejs/aula1/cube.js
@@ -0,0 +1,39 @@
+
+
+
+const renderCube = () => {
+    const rotate = (cube) => {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+    }
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    const scene = new THREE.Scene();
+    const cubewin = document.getElementById("cubeWin")
+    const camera = new THREE.PerspectiveCamera(75, 400/300, 0.1, 400);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(400, 300);
+    
+    cubewin.appendChild(renderer.domElement);
+    scene.add(cube);
+    camera.position.z = 5;
+    const render = () => {
+        requestAnimationFrame(render);
+        rotate(cube)
+        renderer.render(scene, camera);
+    }
+    render();
+
+
+}
+
+
+
+
+
+renderCube()
+
+
+
+
---
+++ treejs/aula1/triangle.js
@@ -0,0 +1,76 @@
+
+
+
+const renderTriangle = () => {
+
+    var geometry = new THREE.BufferGeometry();
+    var hiddenTriangle = new THREE.BufferGeometry();
+    const hiddenTriangleVertices = new Float32Array([
+        0.15, -0.95, 0.0,
+        0.90, -0.7, 0.0,
+        0.65, 0.10, 0.0
+    ]);
+    const vertices = new Float32Array([
+        0.0, 0.0, 0.0,
+        0.5, 0.75, 0.0,
+        1.0, 0.0, 0.0,
+        0.0, 0.0, 0.0,
+        -0.35, -1.0, 0.0,
+        -0.7, 0.25, 0.0,
+        -0.2, 0.15, 0.0,
+        0.35, 0.65, 0.0,
+        -0.85, 0.9, 0.0,
+        0.15, -0.95, 0.0,
+        0.90, -0.7, 0.0,
+        0.65, 0.10, 0.0
+    ]);
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    hiddenTriangle.setAttribute('position', new THREE.BufferAttribute(hiddenTriangleVertices, 3));
+    var colors = new Uint8Array([
+        255, 0, 255,
+        255, 0, 255,
+        255, 0, 255,
+        255, 255, 0,
+        255, 255, 0,
+        255, 255, 0,
+        255, 0, 0,
+        0, 255, 0,
+        0, 0, 255,
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+    ]);
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3, true));
+    const geometryMaterial = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors, side: THREE.DoubleSide });
+    var outlineMaterial1 = new THREE.MeshBasicMaterial({ color: "white", wireframe: true });
+    const triangle = new THREE.Mesh(geometry, geometryMaterial);
+    const triangleoOutline = new THREE.Mesh(hiddenTriangle, outlineMaterial1);
+
+    const scene = new THREE.Scene();
+    const trianglewin = document.getElementById("triangleWin")
+    const camera = new THREE.PerspectiveCamera(75, 400 / 300, 0.1, 400);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(400, 300);
+
+    trianglewin.appendChild(renderer.domElement);
+    scene.add(triangle);
+    scene.add(triangleoOutline);
+    camera.position.z = 5;
+    const render = () => {
+        requestAnimationFrame(render);
+        renderer.render(scene, camera)
+    }
+    render();
+
+
+}
+
+
+
+
+
+renderTriangle()
+
+
+
+
